1e9b0247b0869caa42fa9238b337a7cc6162fadf
Krzysztof Tybinka <ktybinka@interia.pl>
2023-03-05 17:06:46 +0100

    Atribute changed

--- TaskManager/Models/Status.cs
+++ TaskManager/Models/Status.cs
@@ -1,6 +1,10 @@
-﻿namespace TaskManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
 {
     public class Status
     {
+        [Key, StringLength(20)]
+        public string Id { get; set; }
     }
 }
--- TaskManager/Models/Task.cs
+++ TaskManager/Models/Task.cs
@@ -1,6 +1,28 @@
-﻿namespace TaskManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManager.Areas.Identity.Data;
+
+namespace TaskManager.Models
 {
     public class Task
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        //public int Status { get; set; }
+
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        public DateTime? CompletionDate { get; set; } 
+
+        public string PublisherId { get; set; }
+        public virtual TaskManagerUser Publisher { get; set; }
+
+        public string AssigneeId { get; set; }
+        public virtual TaskManagerUser Assignee { get; set; }
+
     }
 }
