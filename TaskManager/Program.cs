be1eb46d08122cf1588d58b44665dc5b1c0a7f8e
Krzysztof Tybinka <ktybinka@interia.pl>
2023-03-05 15:14:31 +0100

    Identity generated

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Areas.Identity.Data;
 namespace TaskManager
 {
     public class Program
@@ -5,6 +8,13 @@ namespace TaskManager
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+                        var connectionString = builder.Configuration.GetConnectionString("AppDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AppDbContextConnection' not found.");
+
+                                    builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+                                                builder.Services.AddDefaultIdentity<TaskManagerUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<AppDbContext>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -23,6 +33,7 @@ namespace TaskManager
             app.UseStaticFiles();
 
             app.UseRouting();
+                        app.UseAuthentication();;
 
             app.UseAuthorization();
 
