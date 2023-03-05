9f36a7a3eb55a2b65305bbbd61b20b781a4e6401
Krzysztof Tybinka <ktybinka@interia.pl>
2023-03-05 14:55:50 +0100

    Properties added

--- TaskManager/Models/Person.cs
+++ TaskManager/Models/Person.cs
@@ -1,6 +1,32 @@
-﻿namespace TaskManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Models
 {
     public class Person
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, MinLength(50)]
+        public string LastName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Login { get; set; }
+
+        [Required, MaxLength(50), NotMapped, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public byte[] PasswordHash { get; set; }
+
+        [Required]
+        public byte[] PasswordSalt { get; set; }
+
+        [Required]
+        public string Role { get;set; }
     }
 }
