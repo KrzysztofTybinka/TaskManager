19673465eff254e647849ea2f6bb821a6189b993
Krzysztof Tybinka <ktybinka@interia.pl>
2023-03-06 01:11:05 +0100

    Insertion removed

--- TaskManager/Areas/Identity/Data/AppDbContext.cs
+++ TaskManager/Areas/Identity/Data/AppDbContext.cs
@@ -23,14 +23,6 @@ public class AppDbContext : IdentityDbContext<TaskManagerUser>
         builder.Entity<IdentityRole>().HasData(
             new IdentityRole() { Id = "e45e7052-fab1-4848-9057-162971c9e740", Name = "Admin", NormalizedName = "ADMIN" },
             new IdentityRole() { Id = "420c2f8f-3315-425f-b865-13e9146991b6", Name = "User", NormalizedName = "USER" });
-
-        builder.Entity<TaskManagerUser>().HasData(
-            new TaskManagerUser() { Id = "f0b2ad1c-f92f-46bf-abb7-170a4ba8d168", UserName = "admin", Email = "admin@admin.com" ,EmailConfirmed = true, PasswordHash = Crypto.HashPassword("Zaq12wsx!"), PhoneNumberConfirmed = true },
-            new TaskManagerUser() { Id = "896ef532-034c-4425-8d71-75f90b17e6fa", UserName = "user", Email = "user@user.com", EmailConfirmed = true, PasswordHash = Crypto.HashPassword("Zaq12wsx!"), PhoneNumberConfirmed = true });
-
-        builder.Entity<IdentityUserRole<string>>().HasData(
-            new IdentityUserRole<string> { UserId = "f0b2ad1c-f92f-46bf-abb7-170a4ba8d168", RoleId = "e45e7052-fab1-4848-9057-162971c9e740" }, 
-            new IdentityUserRole<string> { UserId = "896ef532-034c-4425-8d71-75f90b17e6fa", RoleId = "420c2f8f-3315-425f-b865-13e9146991b6" });
             
         builder.Entity<Status>().HasData(
             new Status() { Id = "To do" },
