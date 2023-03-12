43c93416203d7e9fcc06e2e342fb00a3e7f695a0
Krzysztof Tybinka <ktybinka@interia.pl>
2023-03-12 17:42:24 +0100

    Edit methods restored

--- TaskManager/Controllers/TaskController.cs
+++ TaskManager/Controllers/TaskController.cs
@@ -85,7 +85,7 @@ namespace TaskManager.Controllers
             return View(task);
         }
 
-        // GET: Task/Edit/5
+        // POST: Task/Apply
         public async Task<IActionResult> Apply(int? id)
         {
             if (id == null || _context.Tasks == null)
@@ -103,6 +103,64 @@ namespace TaskManager.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Task/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            ViewData["AssigneeId"] = new SelectList(_context.Users, "Id", "Id", task.AssigneeId);
+            ViewData["PublisherId"] = new SelectList(_context.Users, "Id", "Id", task.PublisherId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", task.StatusId);
+
+            return View(task);
+        }
+
+        // POST: Task/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,StatusId,PublishDate,CompletionDate,PublisherId,AssigneeId")] Task task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AssigneeId"] = new SelectList(_context.Users, "Id", "Id", task.AssigneeId);
+            ViewData["PublisherId"] = new SelectList(_context.Users, "Id", "Id", task.PublisherId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", task.StatusId);
+            return View(task);
         }
 
 
